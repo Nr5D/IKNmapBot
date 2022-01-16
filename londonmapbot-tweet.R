437bc2e18527454bc10589a2416213790ba03a44
Abdul Aziz Nurussadad <56505862+Nr5D@users.noreply.github.com>
2022-01-16 07:07:41 +0700

    Update londonmapbot-tweet.R

--- londonmapbot-tweet.R
+++ londonmapbot-tweet.R
@@ -1,6 +1,6 @@
 # Create Twitter token
 londonmapbot_token <- rtweet::create_token(
-  app = "londonmapbot",
+  app = "Sedotan",
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
